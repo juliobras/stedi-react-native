18ac610bc381bc791f2cb54ea0286661d9eea8d4
Daisy Nadauld <daisykunzler@gmail.com>
2021-12-07 15:29:53 -0700

    labeling

--- Setting.js
+++ Setting.js
@@ -3,7 +3,7 @@ import { StyleSheet, Text, View, Image } from 'react-native';
 import { Card, ListItem, Button, Icon } from 'react-native-elements';
 import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 
-
+/* THIS IS THE LOGIN BUTTON! */
 function Setting() {
     return(
       <View>
