cad08766b7f2733b484be35616347fa34ecc696a
Daisy Nadauld <daisykunzler@gmail.com>
2021-12-07 15:53:09 -0700

    accepted incoming chnages?

--- App.js
+++ App.js
@@ -8,19 +8,12 @@ import Home from './Home.js';
 import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs';
 import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 import FontAwesome from 'react-native-vector-icons/FontAwesome';
-<<<<<<< HEAD
-
-// import Icons from "./Icons";
-const Tab = createMaterialBottomTabNavigator();
-
-=======
 import Login from './Login.js';
 // import Icons from "./Icons";
 const Tab = createMaterialBottomTabNavigator();
 //test Julio...
 //testing again
 //showing Clark
->>>>>>> dd1d0f26d85dd367344b948c8111262fe9f7f0ab
 export default function App() {
   return (
     <NavigationContainer>
