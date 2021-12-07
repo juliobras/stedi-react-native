912877b67bd83be0e7be85bd9952bfda421a085c
Julio <juliocesarmonteiro2.0@outlook.com>
2021-12-07 10:46:30 -0700

    In the login page I am trying to keep the login image with the login button. Somehow the changes I made to the code are not reflecting in the app.

--- Login.js
+++ Login.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import { StyleSheet, Text, View, Image } from 'react-native';
 import { Card, Button } from 'react-native-elements';
-import Icons from './Icons.js';
+import Icons2 from './Icons.js';
 import Bar  from './Bar.js';
 
 const Login = () => {
   return (
     <View style={styles.bar}>
       <Bar />
+      <Icons2 />
      
     </View>
   );
