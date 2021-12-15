3910e5d38b08844c40ca0554e80bc5011a63d62a
AlgothAybara <ay0900356@byui.edu>
2021-12-14 22:04:37 -0700

    Commented out functions for OTP, fixed error

--- TheLoginButton.js
+++ TheLoginButton.js
@@ -16,49 +16,49 @@ function LoginButton() {
     )
   }
 
-  function setusercode() {
-    oneTimePassword = $("#usercode").val();
-    var valid=codeRegEx.exec(oneTimePassword);
-    // if (!valid){
-    //     alert('Must be 4 digits.');
-    // }
-}
+//   function setusercode() {
+//     oneTimePassword = $("#usercode").val();
+//     var valid=codeRegEx.exec(oneTimePassword);
+//     // if (!valid){
+//     //     alert('Must be 4 digits.');
+//     // }
+// }
 
-function setphone() {
-    phoneNumber = $("#phonenumber").val();
-    var valid=phoneRegEx.exec(phoneNumber);
-    // if (!valid){
-    //     alert('Must be 6 digits.');
-    // }
-}
+// function setphone() {
+//     phoneNumber = $("#phonenumber").val();
+//     var valid=phoneRegEx.exec(phoneNumber);
+//     // if (!valid){
+//     //     alert('Must be 6 digits.');
+//     // }
+// }
 
-function generate2FA(){
-    setphone();
-    $.ajax({
-        // type: 'GET',
-        type: 'POST',
-        url: getApiRoot() + '/twofactorlogin/' + phoneNumber,
-        contentType: "application/text",
-        dataType: 'text'
-    });
+// function generate2FA(){
+//     setphone();
+//     $.ajax({
+//         // type: 'GET',
+//         type: 'POST',
+//         url: getApiRoot() + '/twofactorlogin/' + phoneNumber,
+//         contentType: "application/text",
+//         dataType: 'text'
+//     });
 
-}
+// }
 
-function login2FA(){
-    setusercode();
-    setphone();
-    $.ajax({
-        type: 'POST',
+// function login2FA(){
+//     setusercode();
+//     setphone();
+//     $.ajax({
+//         type: 'POST',
 
-        url: getApiRoot() + '/twofactorlogin',
-        data: JSON.stringify({phoneNumber, oneTimePassword}),
-        success: function(data) {
-            window.location.href = "/timer.html#"+data;//add the token to the url
-        },
-        error: console.log(JSON.stringify({phoneNumber, oneTimePassword})),
-        contentType: "application/text",
-        dataType: 'text'
-    });
+//         url: getApiRoot() + '/twofactorlogin',
+//         data: JSON.stringify({phoneNumber, oneTimePassword}),
+//         success: function(data) {
+//             window.location.href = "/timer.html#"+data;//add the token to the url
+//         },
+//         error: console.log(JSON.stringify({phoneNumber, oneTimePassword})),
+//         contentType: "application/text",
+//         dataType: 'text'
+//     });
 
-}
+// }
 export default LoginButton;
